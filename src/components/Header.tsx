7bfa305e3c3337167faf3df07a748d608c06850d
Liberosa <40002607+Liberosa@users.noreply.github.com>
2025-08-02 21:31:04 +0300

    added changes in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,15 @@ import Navigation from "./Navigation.tsx";
 import {useContext} from "react";
 import {SWContext} from "../utils/context.ts";
 import {characters} from "../utils/constants.ts";
+import {useErrorPage} from "../hooks/useErrorPage.tsx";
 
 const Header = () => {
     const {hero} = useContext(SWContext)
+    const {isError} = useErrorPage();
     return (
         <header className="rounded-t-3xl bg-grey-color">
             <Navigation/>
-            <h1 className="text-center text-3xl py-6">{characters[hero].name}</h1>
+            <h1 className="text-center text-3xl py-6">{isError ? "Error" : characters[hero].name}</h1>
         </header>
     );
 };
